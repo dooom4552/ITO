0b8b16fa8e3d4f51abccb49a5c0ed661779548ca
Рита Миннекаева <RitaRita1957@hotmail.com>
2020-04-28 16:51:38 +0300

    Добавьте файлы проекта.

---
+++ ITO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ITO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

5f567f95b40f1b379233d4a996a7d043c95dc9c8
Рита Миннекаева <RitaRita1957@hotmail.com>
2020-05-19 13:17:58 +0300

    Сделан скрипт изменения по кварталам

---
+++ ITO/wwwroot/js/QuaterScript.js
@@ -0,0 +1,21 @@
+﻿const firstQuarter = document.getElementById('FirstQuarter')
+const secondQuarter = document.getElementById('SecondQuarter')
+const thirdQuarter = document.getElementById('ThirdQuarter')
+const fourthQuarter = document.getElementById('FourthQuarter')
+const form = document.getElementById('forgot_pass_form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {   
+    let sum = firstQuarter.value + secondQuarter.value + thirdQuarter.value + fourthQuarter.value;
+    
+    let messages = []
+    if ( sum == 0) {
+        
+        messages.push('Введите значение')
+       
+    }
+    if (messages.length > 0) {
+        alert("Введите значение  по кварталам")
+        e.preventDefault()       
+    }    
+})
